1ca037e62f106703f222a6f09e3faea860c47e79
annTerry <annak@1gb.ru>
2023-01-10 12:29:15 +0300

    feat: set .env and start server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-console.log('Hello world!');
+import * as dotenv from 'dotenv'
+import http from "http";
+
+dotenv.config()
+
+const PORT = process.env.PORT || 4000;
+const server = http.createServer()
+
+server.on('error', (e) => {
+  console.log(e.message);
+});
+
+server.on("request", (request, response) => {
+  switch (request.method) {
+    case "GET":
+      switch (request.url) {
+        default:
+          response.statusCode = 400
+          response.write(`CANNOT GET ${request.url}`)
+          response.end()
+      }
+      break
+
+    case "POST":
+      break
+
+    case "PUT":
+      break
+
+    case "DELETE":
+      break
+
+    default:
+      // Send response for requests with no other response
+      response.statusCode = 400
+      response.write("No Response")
+      response.end()
+  }
+});
+
+server.listen(PORT, () => {
+  // do whatever
+});
